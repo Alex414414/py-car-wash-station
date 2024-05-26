1dcc1a05b71533d0113e2faf7fb1da9871b30c2c
Alex <54539kalashnikov@gmail.com>
2024-05-26 16:42:06 +0300

    Check "car.clean_mark"

--- app/main.py
+++ app/main.py
@@ -17,22 +17,24 @@ class CarWashStation:
     def serve_cars(self, cars: list) -> float:
         fin_sum = 0
         for car in cars:
-            if car.clean_mark < self.clean_power:
-                fin_sum += self.calculate_washing_price(car)
-                self.wash_single_car(car)
+            fin_sum += self.calculate_washing_price(car)
+            self.wash_single_car(car)
         return round(fin_sum, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round((car.comfort_class * (self.clean_power
-                     - car.clean_mark) * self.average_rating
-                     / self.distance_from_city_center), 1)
+        if car.clean_mark < self.clean_power:
+            return round((car.comfort_class * (self.clean_power
+                         - car.clean_mark) * self.average_rating
+                         / self.distance_from_city_center), 1)
+        return 0
 
     '''method, that washes a single car,
     so it should have "clean_mark" equals wash
     station's "clean_power", if "wash_station.clean_power"
     is greater than "car.clean_mark"'''
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> None:
         self.average_rating = round(((self.average_rating
