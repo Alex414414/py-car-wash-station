4d425d5ed3ae6cc5ba76b2a2319d3879f6e61cdd
Alex <54539kalashnikov@gmail.com>
2024-05-26 18:12:20 +0300

    Dell comment and іеÐstyle change

--- app/main.py
+++ app/main.py
@@ -23,15 +23,11 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
-            return round((car.comfort_class * (self.clean_power
-                         - car.clean_mark) * self.average_rating
-                         / self.distance_from_city_center), 1)
+            levels_clean = self.clean_power - car.clean_mark
+            rating_dist = self.average_rating / self.distance_from_city_center
+            return round((car.comfort_class * levels_clean * rating_dist), 1)
         return 0
 
-    '''method, that washes a single car,
-    so it should have "clean_mark" equals wash
-    station's "clean_power", if "wash_station.clean_power"
-    is greater than "car.clean_mark"'''
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
